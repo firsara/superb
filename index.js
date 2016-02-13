e28b0d8b7cc9e15858623dec31e50a29a4ac3682
Fabian Irsara <info@fabianirsara.com>
2016-02-13 15:07:45 +0100

    fix import, add prefix function

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 'use strict';
-var words = require('./words.json');
+var words = require('./words.js');
 var uniqueRandomArray = require('unique-random-array');
+
+var prefixWord = function(){
+	var word = module.exports();
+	var firstChar = word.substring(0, 1);
+
+	if (firstChar === 'a' || firstChar === 'e' || firstChar === 'i' || firstChar === 'o' || firstChar === 'u') {
+		return 'an ' + word;
+	}
+
+	return 'a ' + word;
+};
 
 module.exports = uniqueRandomArray(words);
 module.exports.words = words;
+module.exports.prefix = prefixWord;
